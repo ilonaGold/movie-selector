2ab7e32e8409a3fc2f4b02f7e323edcd783edaf8
ilonaGold <songa17@inbox.lv>
2021-12-27 01:59:59 +0200

    Added base javascript

---
+++ script.js
@@ -0,0 +1,42 @@
+const ageSelector = document.getElementById("age-selector");
+const genreSelector = document.getElementById("genre-selector");
+
+const btn = document.getElementById("btn");
+const suggestedMovie = document.getElementById("suggested-movie");
+
+const moviesArr = [
+    {
+        name: "Die Hard",
+        age: "18+",
+        genre: "Action"
+    },
+    {
+        name: "Love Actually",
+        age: "18+",
+        genre: "Romance"
+    },
+    {
+        name: "The Polar Express",
+        age: "PG",
+        genre: "Action"
+    },
+    {
+        name: "Shrek",
+        age: "PG",
+        genre: "Romance"
+    }
+]
+
+
+function displayMovie () {
+    const selectedAge = ageSelector.value;
+    const selectedMovie = genreSelector.value;
+    for(let i = 0; i < moviesArr.length; i++) {
+        if (moviesArr[i].age === selectedAge && moviesArr[i].genre === selectedMovie) {
+            suggestedMovie.textContent = moviesArr[i].name;
+        }
+    }
+    
+}
+
+btn.addEventListener("click", displayMovie);
